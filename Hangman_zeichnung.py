920c160bb65bfe4fa90bb2c0b809e375fb0a362d
GerJaeger <jaeger_42@gmx.de>
2023-11-07 11:14:57 +0100

    Drawing the first part

---
+++ Hangman_zeichnung.py
@@ -0,0 +1,47 @@
+import turtle
+
+turtle.hideturtle()
+# First
+turtle.penup()
+turtle.right(135)
+turtle.forward(50)
+turtle.pendown()
+turtle.right(180)
+turtle.forward(50)
+
+# second
+turtle.right(90)
+turtle.forward(50)
+
+#third
+turtle.right(180)
+turtle.forward(50)
+turtle.right(45)
+turtle.forward(100)
+
+# fourth
+turtle.right(90)
+turtle.forward(100)
+
+# fifth
+turtle.back(70)
+turtle.right(135)
+turtle.forward(43)
+
+# sixth
+turtle.back(43)
+turtle.left(135)
+turtle.forward(70)
+turtle.right(90)
+turtle.forward(25)
+turtle.left(90)
+
+for i in range(40):
+    turtle.forward(1)
+    turtle.right(9)
+
+# seventh
+turtle.penup()
+turtle.right(90)
+turtle.forward(20)
+    
